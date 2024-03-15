c570690ddb04bdf57e25c50d124cac1f501e1517
Your Name <you@example.com>
2024-03-15 23:03:19 +0530

    modified:   components/About.tsx 	modified:   components/Navbar.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
         <div className="w-full flex flex-col items-center justify-center px-2 py-24 lg:flex-row lg:w-[80%] lg:max-w-[1000px] lg:py-2 lg:pt-16">
             <MovingBorderContainer
                 duration={5000}
-                className="bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.2)]"
+                className="bg-gradient-to-t from-[rgba(0,0,0,0.4)] to-[rgba(0,0,0,0.2)] lg:grow lg:shrink-0 basis-[50%]"
             >
                 <Bio />
             </MovingBorderContainer>
