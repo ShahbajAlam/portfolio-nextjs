7ac76e6ce2a4a7c7bc59e7a9f8f5d946acda41ea
Your Name <you@example.com>
2024-03-15 21:57:25 +0530

    new file:   assets/man.webp 	modified:   components/About.tsx 	new file:   components/Bio.tsx 	new file:   components/ui/moving-border.tsx 	new file:   components/ui/tailwindcss-buttons.tsx 	modified:   package-lock.json 	modified:   package.json 	modified:   tailwind.config.ts

---
+++ components/Bio.tsx
@@ -0,0 +1,29 @@
+import man from "@/assets/man.webp";
+
+function Bio() {
+    return (
+        <div className="flex flex-col items-center gap-3 p-4 rounded-3xl">
+            <img
+                src={man.src}
+                alt="profile picture"
+                className="w-20 h-20 rounded-full border-2 border-orange-800"
+            />
+            <p className="text-lg text-gray-100 text-justify">
+                I am a passionate React Frontend developer dedicated to creating
+                engaging and seamless user experiences with a strong foundation
+                in web and app development, I thrive on turning design concepts
+                into responsive and dynamic interfaces. My goal is to contribute
+                innovative solutions and bring ideas to life through clean and
+                efficient code. Let's build something remarkable together!
+            </p>
+            <a
+                download=""
+                className="self-end bg-gradient-to-r from-[#3e4e6e] to-[#212e41] rounded-xl px-4 py-2 font-semibold text-xl my-2"
+            >
+                Download Resume
+            </a>
+        </div>
+    );
+}
+
+export default Bio;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,8 +28,17 @@ const config: Config = {
             animation: {
                 spotlight: "spotlight 2s ease .75s 1 forwards",
                 "meteor-effect": "meteor 5s linear infinite",
+                shimmer: "shimmer 2s linear infinite",
             },
             keyframes: {
+                shimmer: {
+                    from: {
+                        backgroundPosition: "0 0",
+                    },
+                    to: {
+                        backgroundPosition: "-200% 0",
+                    },
+                },
                 spotlight: {
                     "0%": {
                         opacity: "0",
