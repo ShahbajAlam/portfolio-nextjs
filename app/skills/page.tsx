6d9e5e022d70a0aa182df285fcc89fd69877a26e
Your Name <you@example.com>
2024-03-17 23:03:02 +0530

    modified:   app/skills/page.tsx 	modified:   components/ui/card-hover-effect.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -3,11 +3,13 @@
 import React from "react";
 
 import { skills } from "@/utils/skillsArray";
+import { Meteors } from "@/components/ui/meteors";
 import { HoverEffect } from "@/components/ui/card-hover-effect";
 
 export default function SkillsPage() {
     return (
         <div className="min-h-dvh w-full flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.04] relative overflow-hidden px-2 py-24 lg:py-2 lg:pt-16">
+            <Meteors number={50} />
             <HoverEffect items={skills} />
         </div>
     );
--- components/ui/card-hover-effect.tsx
+++ components/ui/card-hover-effect.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/utils/cn";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
 import { useState } from "react";
 
 export const HoverEffect = ({
@@ -34,7 +33,7 @@ export const HoverEffect = ({
                     <AnimatePresence>
                         {hoveredIndex === idx && (
                             <motion.span
-                                className="absolute inset-0 h-full w-full bg-cyan-600 dark:bg-slate-800/[0.8] block  rounded-3xl"
+                                className="absolute inset-0 h-full w-full bg-cyan-600 dark:bg-slate-800/[0.2] block  rounded-3xl"
                                 layoutId="hoverBackground"
                                 initial={{ opacity: 0 }}
                                 animate={{
@@ -56,7 +55,6 @@ export const HoverEffect = ({
                                 src={item.image}
                                 width={64}
                                 height={64}
-                                priority
                                 className="aspect-square w-16  md:w-24 lg:w-28"
                             />
                         </CardDescription>
@@ -77,7 +75,7 @@ export const Card = ({
     return (
         <div
             className={cn(
-                "rounded-2xl h-full w-full p-4 overflow-hidden bg-black group-hover:border-slate-700 relative z-20",
+                "rounded-2xl h-full w-full p-4 overflow-hidden bg-black relative z-20",
                 className
             )}
         >
