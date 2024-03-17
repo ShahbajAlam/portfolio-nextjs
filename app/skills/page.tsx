ef336cb6dbc659d2102d80b3c5ad264f191f691e
Your Name <you@example.com>
2024-03-17 20:54:41 +0530

    modified:   app/skills/page.tsx 	deleted:    components/ui/background-beams.tsx 	new file:   components/ui/background-boxes.tsx

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,41 +1,15 @@
 "use client";
 
 import React from "react";
-import { HoverEffect } from "@/components/ui/card-hover-effect";
-import { BackgroundBeams } from "@/components/ui/background-beams";
-
-import home from "@/assets/home.webp";
-
-const skills = [
-    {
-        name: "HTML",
-        image: home.src,
-    },
-    {
-        name: "HTML",
-        image: home.src,
-    },
-    {
-        name: "HTML",
-        image: home.src,
-    },
-    {
-        name: "HTML",
-        image: home.src,
-    },
-    {
-        name: "HTML",
-        image: home.src,
-    },
-];
+import { cn } from "@/utils/cn";
+import { Boxes } from "@/components/ui/background-boxes";
 
 export default function SkillsPage() {
     return (
-        <div className="min-h-dvh w-full bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-            <BackgroundBeams />
-            <div className="w-full flex flex-col items-center justify-center px-2 py-24 md:w-[75%] lg:w-[80%] lg:max-w-[1000px] lg:pb-4 lg:pt-24">
-                <HoverEffect items={skills} />
-            </div>
+        <div className="min-h-dvh relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+            <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+            <Boxes />
         </div>
     );
 }
