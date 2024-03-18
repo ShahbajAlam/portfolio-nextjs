29f3174a4abab7ba1306b633bd43aa6e82930f6b
Your Name <you@example.com>
2024-03-18 17:42:31 +0530

    modified:   app/projects/page.tsx 	deleted:    components/ui/3d-pin.tsx 	modified:   utils/projectsArray.ts

---
+++ utils/projectsArray.ts
@@ -0,0 +1,24 @@
+const projects = [
+    {
+        title: "Chatbot",
+        href: "https://shahbajalam-chatbot.netlify.app",
+    },
+    {
+        title: "Expense Tracker",
+        href: "https://shahbajalam-chatbot.netlify.app",
+    },
+    {
+        title: "Chatbot",
+        href: "https://shahbajalam-chatbot.netlify.app",
+    },
+    {
+        title: "Chatbot",
+        href: "https://shahbajalam-chatbot.netlify.app",
+    },
+    {
+        title: "Chatbot",
+        href: "https://shahbajalam-chatbot.netlify.app",
+    },
+];
+
+export { projects };
