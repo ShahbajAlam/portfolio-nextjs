7dd373ebca8fc0f4a9dd9435d8d443c6ebdd6ba1
Your Name <you@example.com>
2024-06-08 20:00:23 +0530

    new file:   assets/grocery.png 	modified:   utils/projectsArray.ts

--- utils/projectsArray.ts
+++ utils/projectsArray.ts
@@ -1,14 +1,14 @@
-import chat from "@/assets/chat.png";
 import budget from "@/assets/budget.png";
+import grocery from "@/assets/grocery.png";
 import chatbot from "@/assets/chatbot.png";
 
 const projects = [
     {
-        title: "Chat Application",
-        tech: "React, Tailwind CSS, Firebase, Framer Motion",
-        href: "https://shahbajalam-chat-app.netlify.app",
-        github: "https://github.com/ShahbajAlam/chat-room-firebase",
-        image: chat.src,
+        title: "Grocery Store",
+        tech: "NextJS, Tailwind CSS, DaisyUI, Kinde, MongoDB, Sanity CMS, Stripe",
+        href: "https://shahbajalam-grocery-store.vercel.app/",
+        github: "https://github.com/ShahbajAlam/grocery-store",
+        image: grocery.src,
     },
     {
         title: "Chat Bot",
