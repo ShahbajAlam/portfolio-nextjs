82326dd97c172f9899fd60423ccc784b87ae4c9b
Shahbaj Alam <shahbajalam78@gmail.com>
2024-09-07 23:59:22 +0530

    updated work

--- components/Work.tsx
+++ components/Work.tsx
@@ -22,7 +22,7 @@ const workArray: WorkProps[] = [
     },
     {
         from: "SEPT, 2024",
-        to: "-",
+        to: "Ongoing",
         company: "byteXL",
         title: "Technical Trainer",
         location: "Dehradun, Uttarakhand",
